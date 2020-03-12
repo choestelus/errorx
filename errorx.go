fb97c8336f80408ab96911434154744d9a112665
Nattapong Amornbunchornvej <nattapong@chochoe.net>
2020-03-12 17:14:43 +0700

    Implement simple error struct

--- errorx.go
+++ errorx.go
@@ -1,1 +1,19 @@
+// Package errorx provides frequently used error constructs in API development
 package errorx
+
+import "fmt"
+
+// E defines common error information for inspecting
+// and displaying to various format
+type E struct {
+	e      error
+	Code   string
+	Fields map[string]interface{}
+}
+
+func (e E) Error() string {
+	if e.e != nil {
+		return e.Error()
+	}
+	return fmt.Sprintf("%+v", e)
+}
